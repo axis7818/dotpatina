018e32721c3046dde7c950d4d891b649d65c8036
axis7818 <axis7818@users.noreply.github.com>
2026-04-26 05:25:49 +0000

    deploy: bdb7ad592443891cff5ef2aa7cd1d058ad717683

---
+++ trait.impl/core/fmt/trait.Display.js
@@ -0,0 +1,9 @@
+(function() {
+    const implementors = Object.fromEntries([["dotpatina",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.94.1/core/fmt/trait.Display.html\" title=\"trait core::fmt::Display\">Display</a> for <a class=\"enum\" href=\"dotpatina/utils/enum.Error.html\" title=\"enum dotpatina::utils::Error\">Error</a>",0]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":59,"fragment_lengths":[274]}
